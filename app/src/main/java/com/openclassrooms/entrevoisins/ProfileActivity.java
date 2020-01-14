f10b4117938134dba1aa6b63b8291493d173bd95
Aimeric David <david.aimeric@outlook.fr>
2020-01-14 19:18:45 +0100

    Commit des init dans ProfileActivity avec le neighbour parcelable et findviewbyID, changement dans le generator neighbour de la qualité de l'image

--- app/src/main/java/com/openclassrooms/entrevoisins/ProfileActivity.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/ProfileActivity.java
@@ -2,17 +2,41 @@ package com.openclassrooms.entrevoisins;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.openclassrooms.entrevoisins.model.Neighbour;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    /** Neighbour */
+    private Neighbour neighbour;
+
+    /** Init object activity_profile */
+    private TextView namePicture;
+    private TextView nameInfo;
+    private ImageView avatarBackground;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        Neighbour neighbour = getIntent().getParcelableExtra("neighbour");
+        /** Get neighbour with Parcelable and Intent */
+        neighbour = getIntent().getParcelableExtra("neighbour");
+
+        /** FindView by id */
+        namePicture = findViewById(R.id.name_neighbour_picture);
+        nameInfo = findViewById(R.id.name_neighbour_info);
+        avatarBackground = findViewById(R.id.avatar_neighbour);
+
+        /** Set Neighbour param of object activity_profile */
+        namePicture.setText(neighbour.getName());
+        nameInfo.setText(neighbour.getName());
+        /** Glide for set picture */
+        Glide.with(ProfileActivity.this).load(neighbour.getAvatarUrl()).into(avatarBackground);
 
     }
 }
