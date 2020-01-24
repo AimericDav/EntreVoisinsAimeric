278da6471755930fd299f41c1408510206049e1a
Aimeric David <david.aimeric@outlook.fr>
2020-01-24 11:55:59 +0100

    Commit
    - Test unitaire fonctionnel

--- app/src/test/java/com/openclassrooms/entrevoisins/service/NeighbourServiceTest.java
+++ app/src/test/java/com/openclassrooms/entrevoisins/service/NeighbourServiceTest.java
@@ -11,8 +11,11 @@ import org.junit.runners.JUnit4;
 
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test on Neighbour service
@@ -39,5 +42,44 @@ public class NeighbourServiceTest {
         Neighbour neighbourToDelete = service.getNeighbours().get(0);
         service.deleteNeighbour(neighbourToDelete);
         assertFalse(service.getNeighbours().contains(neighbourToDelete));
+    }
+
+    @Test
+    public void getFavoriteNeighboursWithSuccess() {
+        assertTrue(service.getFavoriteNeighbours().isEmpty());
+        Neighbour neighbour = service.getNeighbours().get(0);
+        Neighbour neighbour2 = service.getNeighbours().get(1);
+        neighbour.setFavorite(true);
+        neighbour2.setFavorite(true);
+        assertEquals(2, service.getFavoriteNeighbours().size());
+        assertTrue(service.getFavoriteNeighbours().contains(neighbour));
+        assertTrue(service.getFavoriteNeighbours().contains(neighbour2));
+    }
+
+    @Test
+    public void setFavoriteNeighbourWithSuccess() {
+        Neighbour neighbour = service.getNeighbours().get(0);
+        service.setFavoriteNeighbour(neighbour.getId(), true);
+        assertTrue(neighbour.getFavorite());
+        service.setFavoriteNeighbour(neighbour.getId(), false);
+        assertFalse(neighbour.getFavorite());
+    }
+
+    @Test
+    public void getFavoriteNeighbourWithSuccess() {
+        Neighbour neighbour = service.getNeighbours().get(0);
+        assertFalse(service.getFavoriteNeighbour(neighbour.getId()));
+        neighbour.setFavorite(true);
+        assertTrue(service.getFavoriteNeighbour(neighbour.getId()));
+        neighbour.setFavorite(false);
+        assertFalse(service.getFavoriteNeighbour(neighbour.getId()));
+    }
+
+    @Test
+    public void getNeighbourById() {
+        Neighbour neighbourToGet = service.getNeighbours().get(0);
+        Neighbour neighbour = service.getNeighbourById(neighbourToGet.getId());
+        assertEquals(neighbour, neighbourToGet);
+        assertNull(service.getNeighbourById(-1));
     }
 }
